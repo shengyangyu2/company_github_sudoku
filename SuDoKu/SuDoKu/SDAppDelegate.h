b41eb5f1029d05635bc6ea04bb38254679f6e3d6
shengyangyu <784351329@qq.com>
2013-12-23 11:01:51 +0800

    company 2
    company 2

--- SuDoKu/SuDoKu/SDAppDelegate.h
+++ SuDoKu/SuDoKu/SDAppDelegate.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2013年 com.ysy.www. All rights reserved.
 //
 
+/**
+ *  change code
+ *
+ *  @param strong    company mac
+ *  @param nonatomic shengyang yu
+ *
+ *  @return yes
+ */
 #import <UIKit/UIKit.h>
 
 @interface SDAppDelegate : UIResponder <UIApplicationDelegate>
